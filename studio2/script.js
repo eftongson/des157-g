11dacc3d5a50267acb172d57fe368c5478176b99
Emilia Franchet Tongson <etongson@scc2102-16.ucdavis.edu>
2019-02-04 11:47:22 -0800

    changed css, added js

--- studio2/script.js
+++ studio2/script.js
@@ -1,1 +1,16 @@
-console.log(hi hey hello);
+// Emilia Tongson 02-04-19. This is so that windows of information come up once the items are clicked
+'use strict';
+
+console.log('hi hello');
+
+// first initialize and assign items
+var chapstick = document.querySelector('#chapstick');
+var earphones = document.querySelector('#earphones');
+var coughDrop = document.querySelector('#coughDrop');
+
+// initialize and assign global variables
+
+var overlay = document.querySelector('#overlay');
+var close = document.querySelector('#close');
+
+// call addEventListener method on each object so that
