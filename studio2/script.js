67144d90504fab89f0d109fb26f1e608eadafddb
Emilia Franchet Tongson <etongson@scc2102-18.ucdavis.edu>
2019-02-06 10:24:51 -0800

    started over, made js work

--- studio2/script.js
+++ studio2/script.js
@@ -4,13 +4,35 @@
 console.log('hi hello');
 
 // first initialize and assign items
-var chapstick = document.querySelector('#chapstick');
-var earphones = document.querySelector('#earphones');
-var coughDrop = document.querySelector('#coughDrop');
+// var chapstick = document.querySelector('#chapstick');
+// var earphones = document.querySelector('#earphones');
+// var coughDrop = document.querySelector('#coughDrop');
+
+var title = document.querySelector('#title');
 
 // initialize and assign global variables
 
 var overlay = document.querySelector('#overlay');
 var close = document.querySelector('#close');
 
-// call addEventListener method on each object so that
+// call addEventListener method on each object so that it invokes function to execute
+
+// document.getElementById('chapstick').addEventListener('click', function() {
+//   overlay.style.display = 'block';
+// });
+// document.getElementById('earphones').addEventListener('click', function() {
+//   overlay.style.display = 'block';
+// });
+// document.getElementById('coughDrop').addEventListener('click', function() {
+//   overlay.style.display = 'block';
+// })
+
+title.addEventListener('click', function() {
+  overlay.style.display = 'block';
+})
+
+//call addEventListener method on each object so that it invokes function to close
+title.addEventListener('close', function() {
+  console.log('is close an event?');
+  overlay.style.display = 'none';
+})
