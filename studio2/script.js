22932edb5c6744bb078c0bc7a6c5768e1b837609
emilia tongson <eftongson@gmail.com>
2019-02-11 00:48:56 -0800

    added bag, open and close overlays

--- studio2/script.js
+++ studio2/script.js
@@ -6,33 +6,44 @@ console.log('hi hello');
 // first initialize and assign items
 // var chapstick = document.querySelector('#chapstick');
 // var earphones = document.querySelector('#earphones');
-// var coughDrop = document.querySelector('#coughDrop');
+// var coughDrop = document.querySelector('#coughdrop');
 
-var title = document.querySelector('#title');
+var chapstick = document.querySelector('#chapstick');
 
 // initialize and assign global variables
 
-var overlay = document.querySelector('#overlay');
-var close = document.querySelector('#close');
+var chapstickOverlay = document.querySelector('#chapstickOverlay');
+var earphonesOverlay = document.querySelector('#earphonesOverlay');
+var coughdropOverlay = document.querySelector('#coughdropOverlay');
+var description = document.querySelector('.description');
+var close = document.querySelector('.close');
+// temporary hack for close not working in multiple instances
+var close2 = document.querySelector('.close2');
+var close3 = document.querySelector('.close3');
+
 
 // call addEventListener method on each object so that it invokes function to execute
 
-// document.getElementById('chapstick').addEventListener('click', function() {
-//   overlay.style.display = 'block';
-// });
-// document.getElementById('earphones').addEventListener('click', function() {
-//   overlay.style.display = 'block';
-// });
-// document.getElementById('coughDrop').addEventListener('click', function() {
-//   overlay.style.display = 'block';
-// })
-
-title.addEventListener('click', function() {
-  overlay.style.display = 'block';
+chapstick.addEventListener('click', function() {
+  chapstickOverlay.style.display = 'block';
+})
+earphones.addEventListener('click', function() {
+  earphonesOverlay.style.display = 'block';
+})
+coughdrop.addEventListener('click', function() {
+  coughdropOverlay.style.display = "block";
 })
 
-//call addEventListener method on each object so that it invokes function to close
+//call addEventListener method on X symbol so that it invokes function to close
 close.addEventListener('click', function() {
-  console.log('is close an event?');
-  overlay.style.display = 'none';
+  chapstickOverlay.style.display = 'none';
+})
+
+// temporary hack for close not working in multiple instances
+close2.addEventListener('click', function() {
+  earphonesOverlay.style.display = 'none';
+})
+
+close3.addEventListener('click', function() {
+  coughdropOverlay.style.display = 'none';
 })
