2573f5bc76b11c6c8f6ab1652138837c05b94a0d
Emilia Franchet Tongson <etongson@scc2102-16.ucdavis.edu>
2019-03-11 09:55:55 -0700

    almost get feed set up

---
+++ process/development/proto2/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+console.log('feed me');
+
+/* Captures the video camera and adds a filter
+   Run local sever to see in Firefox
+   */
+
+var capture;
+
+var imageXY = 375;
+
+function setup() {
+
+  createCanvas(imageXY, imageXY);
+  // create video capture
+  capture = createCapture(VIDEO);
+  // video capture size
+  capture.size(imageXY, imageXY);
+  capture.hide(); // remove comment to hide video
+
+}
+
+function draw() {
+  background(255);
+  image(capture, 0, 0, 640, 480);
+  filter('INVERT');// change filters: THRESHOLD, GRAY, OPAQUE, INVERT, POSTERIZE, BLUR, ERODE, DILATE or BLUR.
+  capture.parent('myPost1');
+
+}
